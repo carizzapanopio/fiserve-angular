288bd1032e84a8f3530ac703850ea2f61f4b7f81
carizza <carizza.panopio@mrgeekmsi.com>
2018-07-29 22:00:01 +0800

    serverr.js update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fiserve'));
+app.use(express.static('/dist/fiserve'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fiserve/index.html'));
+res.sendFile(path.join('/dist/fiserve/index.html'));
 });
 
 
