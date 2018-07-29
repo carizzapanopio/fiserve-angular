d74ab110a3275b5ac80307d16312ec2df7abb556
carizza <carizza.panopio@mrgeekmsi.com>
2018-07-29 21:25:25 +0800

    update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 
 app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
