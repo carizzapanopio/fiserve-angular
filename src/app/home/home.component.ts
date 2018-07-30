b143f880ae5debe48f77966a7aee6e9596a64087
carizza panopio <carizzapanopio@gmail.com>
2018-07-30 20:21:06 +0800

    validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,6 @@ import {Observable, Subject, merge} from 'rxjs';
 import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
 import { Currency } from '../Models/currency';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';  
-// import {FormGroup, FormControl, Validators} from '@angular/forms';
  
  
 
@@ -21,6 +20,7 @@ export class HomeComponent implements OnInit {
 
   public currency : Currency = new Currency("");
   public convertedRate : String[];
+  public errorMessages : String[];
   // public API_URL : string = 'http://laravel-xoed.frb.io/api/';
   public API_URL : string = "http://localhost/fiserve/public/api/";
 
@@ -30,7 +30,6 @@ export class HomeComponent implements OnInit {
   }
 
   getData(){
-    console.log(this.API_URL + 'rates/currencies');
     this.http.get<Currency[]>(this.API_URL + 'rates/currencies').subscribe(result => {
       if(result.length > 0){
         this.currencyList = [];
@@ -38,14 +37,14 @@ export class HomeComponent implements OnInit {
           let model : Currency = new Currency(result[i].code);
           this.currencyList.push(model.code); 
         }
+      }else{
+        this.currencyList = ['None'];
       }
      });
   }
 
   ngOnInit(): void{
-    // this.currency = new FormGroup({
-      
-    // })
+    this.getData();
   }
   
   @ViewChild('instance') instance: NgbTypeahead;
@@ -63,61 +62,100 @@ export class HomeComponent implements OnInit {
   }
 
   updateRates()  {  
-
-    console.log(this.API_URL + 'rates/fetch');
     this.http.get(this.API_URL + "rates/fetch").subscribe(data =>{
       alert("Rates Updated !");
     });
   }
 
-  convert(){
-    console.log(this.API_URL + "rates/convert");
-    const httpOptions = {
-      headers: new HttpHeaders({
-       
-        'Content-Type': 'application/json', 
-        'Accept': 'application/json' 
-      })
-    };
-    
-    // Initialize Params Object
-    let Params = new HttpParams();
-    // console.log(this.currency.cur_date.join(''));
-    // Begin assigning parameters
+  convert($event){
 
-    // Params = Params.append('amount', this.currency.amount);
-    // Params = Params.append('currency', this.currency.code);
-    // Params = Params.append('published_at', this.currency.cur_date.year + "-" +  this.currency.cur_date.month + "-" +  this.currency.cur_date.day);
-    // let publishedAt = "";
-    // var obj = this.currency.cur_date;
-    // if(Object.keys(obj).length != 0){
-    //   console.log('yow');
-    //   publishedAt = this.currency.cur_date.year + "-" +  this.currency.cur_date.month + "-" +  this.currency.cur_date.day;
-    // }
-    // Params = Params.append("amount", "500");
-    // Params = Params.append("currency", "AFN");
-    // Params = Params.append("published_at", "2018-07-26" );
-    // console.log(Params.toString());
-    // let currency = this;
-    let published_at = Object.values(this.currency.cur_date).join('-');
-     console.log(published_at);
-     this.http.post(this.API_URL + "rates/convert",{ 
-            params : {amount: this.currency.amount, currency: this.currency.code, published_at: published_at }
-            ,httpOptions , observe: 'response'
+    if(this.validateFields($event)){
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+         
+          'Content-Type': 'application/json', 
+          'Accept': 'application/json' 
+        })
+      };
+
+      let published_at = Object.values(this.currency.cur_date).join('-');
+
+      this.http.post(this.API_URL + "rates/convert",{ 
+            params : {
+              amount: this.currency.amount,
+              currency: this.currency.code, 
+              published_at: published_at 
+            },
+            httpOptions,
+            observe: 'response'
           }).subscribe(data => {
-            // alert(JSON.stringify(data));
             this.currency.converted_rate = JSON.stringify(data);
+
+          }, error => {
+            this.errorMessages = [];
+            for (var key in error.error) {
+              this.errorMessages.push( error.error[key][0].replace('params.','') );
+            }
           });
 
+    }
+    
   }
 
+
   // Format Number
-  validateAmount(){
+  validateAmount($event){
     var valid = /^\d{0,4}(\.\d{0,2})?$/.test(this.currency.amount),
     val = this.currency.amount;
- 
-    if(!valid){ 
-        this.currency.amount = val.substr(0, val.length - 1);
+  
+    if(!valid) { 
+      var val_split = val.toString().split('.',2);
+
+      if(val_split[1].length >= 2){
+        this.currency.amount = val_split[0] + "." + val_split[1].substr(0,2);
+      }
+    }
+    this.convert($event);
+    
+  }
+
+
+  validateFields($event){
+    this.errorMessages = [];
+
+      console.log(this.currency.code);
+    if(! this.currency.cur_date){
+      this.errorMessages.push("The <b>date</b> field is required.");
+    }
+
+    if(! this.currency.code){
+      this.errorMessages.push("The <b>code</b> field is required.");
+    }
+
+    if(! this.currency.amount){
+      this.errorMessages.push("The <b>amount</b> field is required.");
+    }
+
+    if(this.errorMessages.length == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  clear(){
+    this.errorMessages = [];
+    this.currency.converted_rate = null;
+  }
+
+  valueChange($event){
+    if(this.currency.cur_date || this.currency.code || this.currency.amount){
+      this.currency.converted_rate = null;
+      this.convert($event);
+    }else{
+      this.currency.converted_rate = null;
     }
   }
 }
+
